a55eec07318b58003ca88db9d36974a543f7f1cd
swapna p <swapnapin23@gmail.com>
2025-08-13 21:20:59 -0400

    apicreate

---
+++ test_apicreate.py
@@ -0,0 +1,10 @@
+import requests
+def test_apicreate():
+    payload = {
+        "title": "Learn Api Testing",
+        "body": "pytest + requests",
+        "userid": 1
+              }
+    response = requests.post("https://jsonplaceholder.typicode.com/posts", json = payload)
+    assert response.status_code == 201
+    assert response.json()["title"] == "Learn Api Testing"
