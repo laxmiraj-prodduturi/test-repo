2e1a4ca0cde5e4a1d3e800c73962da597d78dedc
laxmiraj prodduturi <laxmiraj.prodduturi@gmail.com>
2025-08-13 19:42:09 -0700

    commit in branch

--- test_apicreate.py
+++ test_apicreate.py
@@ -8,6 +8,7 @@ def test_apicreate():
 
     print("Laxmi")
     print("Hello world")
+    print("hello world in branch")
     response = requests.post("https://jsonplaceholder.typicode.com/posts", json = payload)
     assert response.status_code == 201
     assert response.json()["title"] == "Learn Api Testing"
