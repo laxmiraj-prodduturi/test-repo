7b9a84dac04ae17d43a9d40f3f8b738da89c0af3
laxmiraj prodduturi <laxmiraj.prodduturi@gmail.com>
2025-08-13 19:45:04 -0700

    test 1 swp

--- test_apicreate.py
+++ test_apicreate.py
@@ -8,7 +8,7 @@ def test_apicreate():
 
     print("Laxmi")
     print("Hello world")
-    print("hello world in branch")
+    print("hello world in branch swp")
     response = requests.post("https://jsonplaceholder.typicode.com/posts", json = payload)
     assert response.status_code == 201
     assert response.json()["title"] == "Learn Api Testing"
